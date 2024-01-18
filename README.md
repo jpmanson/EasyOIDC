f2d5d7582c9c5966c130d9e160c47516a0356644
juanpablo.manson <juanpablo.manson@wiener-lab.com>
2024-01-18 06:41:08 -0300

    - Added License and setup.py

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 EasyOIDC - Work in progress
+
+References: 
+https://github.com/dnplus/streamlit-oauth
+https://medium.com/streamlit/oauth-component-for-streamlit-e05f00874fbc
+https://dash.plot.ly/authentication
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from pathlib import Path
+
+README = (Path(__file__).parent/"README.md").read_text()
+
+setuptools.setup(
+    name="EasyOIDC",
+    version="0.1.0",
+    author="Juan Pablo Manson",
+    author_email="jpmanson@gmail.com",
+    description="Easy integration with OIDC authentication servers",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jpmanson/EasyOIDC",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.9",
+    license_files=("LICENSE",),
+    install_requires=[
+        "authlib>=1.3.0",
+        "requests>=2.31.0",
+        "python-decouple>=3.8"
+    ],
+)
